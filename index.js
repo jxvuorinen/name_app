d756b1391fac26181a5ad0d379513897bff3acdf
Johanna Vuorinen <johanna.vuorinen@helsinki.fi>
2021-01-05 23:14:21 +0200

    buildui

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('build'))
 const names = require('./names.json');
 
 app.get('/', (req, res) => {
