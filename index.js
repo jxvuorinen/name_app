0b3f06bb10b9c0fa2d81761dc01d52eae9259a30
Johanna Vuorinen <johanna.vuorinen@helsinki.fi>
2021-01-05 22:14:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const names = require('./names.json');
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Solita name-app</h1>')
+})
+
+app.get('/api/names', (req, res) => {
+    res.json(names)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
